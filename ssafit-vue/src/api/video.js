ac7fb911bb672b9de83a5134f904959d383a3be9
DESKTOP-3O24HHM\hitedin <hitedin@gmail.com>
2022-05-16 16:25:46 +0900

    kakao map api 연습

---
+++ ssafit-vue/src/api/video.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const ROOT_URL = "http://localhost:8888/api/";
+
+export default {
+  getVideoList(params) {
+    return axios.get(ROOT_URL + `video`, { params });
+  },
+  getVideo(youtubeId) {
+    return axios.get(ROOT_URL + `video/${youtubeId}`);
+  },
+  createVideo(video) {
+    return axios.post(ROOT_URL + `video/`, video);
+  }
+}
