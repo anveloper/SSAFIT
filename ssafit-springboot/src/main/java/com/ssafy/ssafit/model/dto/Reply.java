22c8065bf1c918dad751579f01ee9c773e5bcf39
WoowonKim <dyffh1031@gmail.com>
2022-05-18 14:50:54 +0900

    change review to reply

--- ssafit-springboot/src/main/java/com/ssafy/ssafit/model/dto/Reply.java
+++ ssafit-springboot/src/main/java/com/ssafy/ssafit/model/dto/Reply.java
@@ -3,16 +3,14 @@ package com.ssafy.ssafit.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
-@ToString
 @NoArgsConstructor
-public class Review {
-	private int reviewSeq;
+public class Reply {
+	private int replySeq;
+	private int reSeq;
 	private String youtubeId;
 	private String writer;
-	private String title;
 	private String content;
 }
