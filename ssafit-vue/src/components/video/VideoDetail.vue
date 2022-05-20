22402e40a2e8d3c4e0810328973aad7cbc53a6a1
DESKTOP-3O24HHM\hitedin <hitedin@gmail.com>
2022-05-20 15:08:16 +0900

    videodetail

--- ssafit-vue/src/components/video/VideoDetail.vue
+++ ssafit-vue/src/components/video/VideoDetail.vue
@@ -21,7 +21,7 @@
         <b-button variant="info" @click="createReply">댓글 등록</b-button>
       </b-input-group-append>
     </b-input-group>
-    <h4 v-else style = "padding-top: 10px ">댓글을 작성하려면 로그인 하세요!</h4>
+    <h4 v-else style="padding-top: 10px">댓글을 작성하려면 로그인 하세요!</h4>
     <b-list-group>
       <b-list-group-item
         v-for="(repl, index) in reply"
@@ -53,21 +53,37 @@
           </b-input-group-append>
         </b-input-group>
         <p class="mb-1" :id="`repl${index}`" v-else>{{ repl.content }}</p>
-        <b-button variant="info" v-if="logonMember.userId" size="sm"  v-b-toggle="`collapse-${index}`" @click="setReComIdx(index)">답글</b-button>
-        <b-collapse :id="`collapse-${index}`" v-if="index==reCommentIdx">
+        <b-button
+          variant="info"
+          v-if="logonMember.userId"
+          size="sm"
+          v-b-toggle="`collapse-${index}`"
+          @click="setReComIdx(index)"
+          >답글</b-button
+        >
+        <b-collapse :id="`collapse-${index}`" v-if="index == reCommentIdx">
           <b-card>
             <b-input-group class="mt-3" style="font-family: initial">
-              <b-form-input :placeholder="`${repl.writer}에게 답글쓰기...`" v-model="reComment"></b-form-input>
+              <b-form-input
+                :placeholder="`${repl.writer}에게 답글쓰기...`"
+                v-model="reComment"
+              ></b-form-input>
               <b-input-group-append>
-                <b-button variant="info" @click="createReReply(repl)">등록</b-button>
+                <b-button variant="info" @click="createReReply(repl)"
+                  >등록</b-button
+                >
               </b-input-group-append>
             </b-input-group>
           </b-card>
         </b-collapse>
         <b-list-group>
-          <b-list-group-item v-for="rerepl in rereply" :key="rerepl.replySeq"  v-show="repl.replySeq == rerepl.reSeq">
-            <h5 style="font-size:0.8rem" class="mb-1">{{ rerepl.writer }}</h5>
-            <div>{{rerepl.content}}</div>
+          <b-list-group-item
+            v-for="rerepl in rereply"
+            :key="rerepl.replySeq"
+            v-show="repl.replySeq == rerepl.reSeq"
+          >
+            <h5 style="font-size: 0.8rem" class="mb-1">{{ rerepl.writer }}</h5>
+            <div>{{ rerepl.content }}</div>
           </b-list-group-item>
         </b-list-group>
       </b-list-group-item>
@@ -82,8 +98,8 @@ export default {
     return {
       items: [],
       comment: "",
-      reComment:"",
-      reCommentIdx:"-1",
+      reComment: "",
+      reCommentIdx: "-1",
       writer: "",
       youtubeId: "",
       updateComment: "-1",
@@ -92,12 +108,12 @@ export default {
   },
   computed: {
     ...mapState(["video", "logonMember"]),
-    reply () {
-      return this.$store.getters.rootReply
+    reply() {
+      return this.$store.getters.rootReply;
     },
-    rereply (){
-      return this.$store.getters.reReply
-    }
+    rereply() {
+      return this.$store.getters.reReply;
+    },
   },
   created() {
     const pathName = new URL(document.location).pathname.split("/");
@@ -119,12 +135,12 @@ export default {
         alert("내용을 입력하세요");
       }
     },
-    createReReply(repl){
+    createReReply(repl) {
       let newReply = {
         content: this.reComment,
         writer: this.logonMember.userId,
         youtubeId: this.video.youtubeId,
-        reSeq: repl.replySeq
+        reSeq: repl.replySeq,
       };
       if (newReply.content) {
         this.$store.dispatch("createReply", newReply);
@@ -132,10 +148,10 @@ export default {
       } else {
         alert("내용을 입력하세요");
       }
-      this.reComment = ""
+      this.reComment = "";
     },
-    setReComIdx(index){
-      this.reCommentIdx = index
+    setReComIdx(index) {
+      this.reCommentIdx = index;
     },
     deleteReply(replySeq) {
       console.log(replySeq);
