b9e36c3024bc72caac0858d458ecd14af7984d79
WoowonKim <dyffh1031@gmail.com>
2022-05-23 14:52:48 +0900

    .

--- ssafit-vue/src/api/youtube.js
+++ ssafit-vue/src/api/youtube.js
@@ -6,17 +6,17 @@ const baseURL = "https://www.googleapis.com/youtube/v3/search?"
   + "&maxResults=10"
   + "&regionCode=KR&q="
 
-const axiosService = axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3/videos?&key=" + process.env.VUE_APP_YOUTUBE_KEY
-    + "& part=snippet&maxResults=10&q=",
-  headers: {
-    "Content-Type": "application/json",
-  }
-});
+// const axiosService = axios.create({
+//   baseURL: "https://www.googleapis.com/youtube/v3/videos?&key=" + process.env.VUE_APP_YOUTUBE_KEY
+//     + "& part=snippet&maxResults=10&q=",
+//   headers: {
+//     "Content-Type": "application/json",
+//   }
+// });
 
 export default {
   getYoutubeApi() {
     let value = encodeURI("헬스");
-    return axiosService.get(baseURL + value);
+    return axios.get(baseURL + value);
   }
 }
